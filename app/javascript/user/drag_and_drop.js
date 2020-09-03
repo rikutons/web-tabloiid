60f607ec612679b38ca2e738bcd713e894d4d4a0
rikutons <rikutonsicloud@gmail.com>
2020-09-03 13:50:25 +0900

    Fix to show message if user upload file by input form

--- app/javascript/user/drag_and_drop.js
+++ app/javascript/user/drag_and_drop.js
@@ -2,7 +2,6 @@ function startAcceptFiles(params) {
     var pdfDropZone = document.getElementById('pdf-drop-zone');
     var pdfFileInput = document.getElementById('pdf-file-input');
 
-    console.log(pdfDropZone)
     pdfDropZone.addEventListener('dragover', function (e) {
         e.stopPropagation();
         e.preventDefault();
@@ -21,10 +20,13 @@ function startAcceptFiles(params) {
         this.style.background = '#ffffff'; //背景色を白に戻す
         var files = e.dataTransfer.files; //ドロップしたファイルを取得
         if (files.length > 1) return alert('アップロードできるファイルは1つだけです。');
-        this.style.background = '#c4b3d4';
-        pdfDropZone.innerText = 'アップロード済み'
         pdfFileInput.files = files; //inputのvalueをドラッグしたファイルに置き換える。
+        onUploaded(pdfDropZone);
     }, false);
+
+    pdfFileInput.addEventListener('change', function () {
+        onUploaded(pdfDropZone);
+    });
 
     var imageDropZone = document.getElementById('image-drop-zone');
     var imageFileInput = document.getElementById('image-file-input');
@@ -48,10 +50,18 @@ function startAcceptFiles(params) {
         this.style.background = '#ffffff'; //背景色を白に戻す
         var files = e.dataTransfer.files; //ドロップしたファイルを取得
         if (files.length > 1) return alert('アップロードできるファイルは1つだけです。');
-        this.style.background = '#c4b3d4';
-        pdfDropZone.innerText = 'アップロード済み'
         imageFileInput.files = files; //inputのvalueをドラッグしたファイルに置き換える。
+        onUploaded(imageDropZone);
     }, false);
+
+    imageFileInput.addEventListener('change', function () {
+        onUploaded(imageDropZone);
+    });
 }
 
 window.onload = startAcceptFiles;
+
+function onUploaded(dropZone) {
+    dropZone.style.background = '#c4b3d4';
+    dropZone.innerText = 'アップロード済み'
+}
